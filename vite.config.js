d875e394209af8734015d4123f4f5fcb9ed69efa
karunjangid <jangidkind@gmail.com>
2025-03-26 11:09:59 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['axios'],
+  resolve: {
+    alias: {
+      '@axios': 'node_modules/axios',
     },
   },
 });
-
